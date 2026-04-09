efd96987f205128fb7573eb6d76bfb4acaaa7f89
Alusi <alusi.shar@gmail.com>
2026-04-08 17:02:48 -0700

    push the remaining last commits

--- terraform/modules/security-groups/main.tf
+++ terraform/modules/security-groups/main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "bastion" {
 
 resource "aws_security_group" "private" {
   name        = "${var.project_name}-private-sg"
-  description = "Allow SSH from bastion only"
+  description = "Allow SSH, monitoring, and Swarm traffic"
   vpc_id      = var.vpc_id
 
   ingress {
@@ -45,6 +45,38 @@ resource "aws_security_group" "private" {
   }
 
   ingress {
+    description = "Docker Swarm management"
+    from_port   = 2377
+    to_port     = 2377
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    description = "Docker Swarm gossip TCP"
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    description = "Docker Swarm gossip UDP"
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "udp"
+    self        = true
+  }
+
+  ingress {
+    description = "Docker Swarm overlay"
+    from_port   = 4789
+    to_port     = 4789
+    protocol    = "udp"
+    self        = true
+  }
+
+  ingress {
     description     = "Prometheus UI from bastion"
     from_port       = 9090
     to_port         = 9090
@@ -56,6 +88,14 @@ resource "aws_security_group" "private" {
     description     = "Grafana UI from bastion"
     from_port       = 3000
     to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion.id]
+  }
+
+  ingress {
+    description     = "Swarm service test from bastion"
+    from_port       = 8088
+    to_port         = 8088
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion.id]
   }
